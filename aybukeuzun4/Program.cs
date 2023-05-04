3438196acefd34af1d44098a5fa1b9d08a0e2705
aybukeuzun <a.uzun200900@hotmail.com>
2023-05-04 18:46:28 +0300

    week4

---
+++ aybukeuzun4/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+Random rnd = new Random();
+
+int myRandom;
+
+for (int i = 0; i < 8; i++)
+{
+    myRandom = rnd.Next(1, 21);
+    Console.WriteLine($"{i + 1}. {myRandom}");
+}
+
+
+
+
+
+
+//for (int i = 0; i < 10; i++) // i++ ==> i + 1
+//{
+//    Console.WriteLine($"{i + 1}. I will not skip Zoom classes.");
+//}
+
---
+++ aybukeuzun44/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+Random rnd = new Random();
+int cpuRandom = rnd.Next(1, 5);
+
+bool loopActive = true; 
+
+int numberofTries = 0;
+
+while (loopActive)
+{
+    Console.WriteLine("Try to guess the cpu no:");
+    int userGuess = Convert.ToInt32(Console.ReadLine());
+
+    if (userGuess == cpuRandom)
+    {
+        Console.WriteLine($"you won!");
+        loopActive = false;
+    }
+}
+
+Console.WriteLine("have a nice day!");
